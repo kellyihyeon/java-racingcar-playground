9e49d0d6ed4026c85cba2ebab2779093e6ce30fd
kellykang-tech <kellykang13@gmail.com>
2022-07-22 00:47:11 +0900

    feat(stringCalculator): 커스텀  구분자를 기준으로 분리한 뒤 그 합을 반환하는 기능 구현

--- src/main/java/com/playground/StringCalculator.java
+++ src/main/java/com/playground/StringCalculator.java
@@ -1,10 +1,24 @@
 package com.playground;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
     private final String[] strArr;
 
+
     public StringCalculator(String inputString) {
-        strArr = inputString.split(",|:");
+        Pattern pattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(inputString);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            strArr = matcher.group(2).split(customDelimiter);
+        } else {
+            strArr = inputString.split(",|:");
+        }
+
     }
 
     public int plus() {
--- src/test/java/com/playground/StringCalculatorTest.java
+++ src/test/java/com/playground/StringCalculatorTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringCalculatorTest {
 
     @Test
+    @DisplayName("커스텀 구분자를 기준으로 문자열을 분리한 뒤 그 합을 반환한다.")
+    void 커스텀_구분자를_기준으로_문자열_분리() {
+        StringCalculator calc = new StringCalculator("//;\n1;2;3");
+        assertEquals(6, calc.plus());
+    }
+
+    @Test
     @DisplayName("기본 구분자 콤마와 콜론을 기준으로 문자열을 분리한 뒤 그 합을 반환한다.")
     void 콤마와_콜론을_기준으로_문자열_분리() {
         StringCalculator calc = new StringCalculator("1,2:3");
