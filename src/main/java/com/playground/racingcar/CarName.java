ed151ccfda14f2cb7624087f77ccc38dd2f43644
kellykang-tech <kellykang13@gmail.com>
2022-07-27 13:45:27 +0900

    feat(carName): 자동차 이름이 5자 이상인 경우 예외를 throw 하는 기능 구현

---
+++ src/main/java/com/playground/racingcar/CarName.java
@@ -0,0 +1,21 @@
+package com.playground.racingcar;
+
+public class CarName {
+
+    private final String name;
+
+    public CarName(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/com/playground/CarNameTest.java
@@ -0,0 +1,26 @@
+package com.playground;
+
+import com.playground.racingcar.CarName;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CarNameTest {
+
+    @Test
+    @DisplayName("자동차 이름이 5자를 초과하면 IllegalArgumentException 예외가 발생한다.")
+    void 자동차_이름이_5자_이상인_경우() {
+        assertThrows(IllegalArgumentException.class, () -> new CarName("golden"));
+    }
+
+    @Test
+    @DisplayName("각 자동차에 이름을 부여할 수 있다.")
+    void getName() {
+        CarName prism = new CarName("prism");
+        CarName green = new CarName("green");
+
+        assertEquals("prism", prism.getName());
+        assertEquals("green", green.getName());
+    }
+}
