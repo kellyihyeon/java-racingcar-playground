da4e318ae339f445ae07477c3f55969c1923dc65
kellykang-tech <kellykang13@gmail.com>
2022-07-27 18:08:42 +0900

    feat(cars): random 값이 음수이거나 9를 초과하면 예외가 발생하는 기능 구현

--- src/main/java/com/playground/racingcar/Car.java
+++ src/main/java/com/playground/racingcar/Car.java
@@ -1,6 +1,6 @@
 package com.playground.racingcar;
 
-public class Car {
+public class Car implements Comparable<Car>{
 
     private final CarName name;
     private int position;
@@ -12,12 +12,33 @@ public class Car {
     }
 
     public void move(int condition) {
+        validateCondition(condition);
         if (condition >= 4) {
             position++;
         }
     }
 
+    private void validateCondition(int condition) {
+        if (isConditionWrongNumber(condition)) {
+            throw new IllegalArgumentException("자동차가 이동하기 위한 랜덤 값은 0이상 9이하이어야 합니다.");
+        }
+    }
+
+    private boolean isConditionWrongNumber(int condition) {
+        return condition < 0 || condition > 9;
+    }
+
     public int getPosition() {
         return position;
     }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.position - o.position;
+    }
+
 }
--- src/test/java/com/playground/CarTest.java
+++ src/test/java/com/playground/CarTest.java
@@ -2,16 +2,32 @@ package com.playground;
 
 import com.playground.racingcar.Car;
 import com.playground.racingcar.CarName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarTest {
 
+    private Car prism;
+
+    @BeforeEach
+    void setUp() {
+        prism = new Car(new CarName("prism"));
+    }
+
+    @DisplayName("랜덤값이 음수이거나 9를 초과하면 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest(name = "{index} 랜덤값이 {0} 이면 IllegalArgumentException 예외가 발생한다.")
+    @ValueSource(ints = {-1, 10})
+    void 랜덤값이_음수이거나_9를_초과하는_경우(int inputNumber) {
+        assertThrows(IllegalArgumentException.class, () -> prism.move(inputNumber));
+    }
+
     @Test
     @DisplayName("random 값이 4 이상일 경우 전진한다.")
     void 랜덤_값이_4이상인_경우() {
-        Car prism = new Car(new CarName("prism"));
         prism.move(4);
         assertEquals(2, prism.getPosition());
     }
@@ -19,7 +35,6 @@ public class CarTest {
     @Test
     @DisplayName("random 값이 4 미만일 경우 제자리에 있는다.")
     void 랜덤_값이_4미만인_경우() {
-        Car prism = new Car(new CarName("prism"));
         prism.move(3);
         assertEquals(1, prism.getPosition());
     }
